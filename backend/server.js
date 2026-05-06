48cc02ed01d9bd99ad75bbc99796d25c5569ef1b
Tigmamanyu Sahoo <tigmamanyusahoo@gmail.com>
2026-05-06 12:23:59 +0530

    Debugging redis connection issue at prod

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const dotenv = require('dotenv');
 // INJECTING ENV VARIABLES
 dotenv.config();
+const logger = require('./src/utils/logger');
+
+logger.info(`REDIS_HOST: ${process.env.REDIS_HOST}`);
+logger.info(`REDIS_PORT: ${process.env.REDIS_PORT}`);
+logger.info(`REDIS_PASSWORD: ${process.env.REDIS_PASSWORD}`);
 
 const app = require('./app');
 const connectDB = require('./src/config/DBconnection');
 
 // JOBS
 const {startStreakResetJob} = require('./src/jobs/streakReset.job');
-
-// UTILS
-const logger = require('./src/utils/logger');
 
 // CONNECT TO THE DB
 connectDB().then(() => {
